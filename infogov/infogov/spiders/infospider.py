36fec0f4490c330a768a0a99d972f7933ab625a6
Aditya <156055539+AdityaAdi07@users.noreply.github.com>
2024-11-19 20:28:44 +0530

    Update infospider.py

--- infogov/infogov/spiders/infospider.py
+++ infogov/infogov/spiders/infospider.py
@@ -5,45 +5,32 @@ class InfospiderSpider(scrapy.Spider):
     allowed_domains = ["www.india.gov.in"]
     start_urls = ["https://www.india.gov.in/people-groups/life-cycle/senior-citizens"]
 
+    def __init__(self, state='', *args, **kwargs):
+        super(InfospiderSpider, self).__init__(*args, **kwargs)
+        self.state = state.lower()
+
     def parse(self, response):
-        
-        h3_elements = response.xpath('//*[@id="metadata_3rd_level"]/div[2]/div/div[1]/div/div/div/div/ol//h3')
+        li_elements = response.xpath('//*[@id="metadata_3rd_level"]/div[2]/div/div[5]/div/div/div/div[2]/ol/li')
 
-        for h3 in h3_elements:
-            
-            h3_text = h3.xpath('.//a/text()').get(default='').strip()
-
-            
-            url = h3.xpath('.//a/@href').get()
-            if url:
-                
-                url = response.urljoin(url)
-
-           
-            yield {
-                'text': h3_text,
-                'url': url
-            }
-
-        
-        li_elements = response.xpath('//*[@id="metadata_3rd_level"]/div[2]/div/div[3]/div/div/div/div[2]/ol/li')
         for li in li_elements:
-           
             text = li.xpath('.//a/text()').get(default='').strip()
             url = li.xpath('.//a/@href').get()
             if url:
                 url = response.urljoin(url)
-
-            
             abt = li.xpath('.//div[contains(@class, "field-content")]/p/text()').get(default='').strip()
 
-           
+      
+            is_highlighted = self.state in abt.lower()
+
             yield {
                 'SCHEME': text,
                 'URL': url,
-                'DESCRIPTION': abt
+                'DESCRIPTION': abt,
+                'HIGHLIGHT': is_highlighted  
             }
+
+    
         next_page = 'https://www.india.gov.in/people-groups/life-cycle/senior-citizens?page='
-        for i in range (2,5):
-            next_page_url = next_page+f'{i}'
+        for i in range(0, 4):
+            next_page_url = next_page + f'{i}'
             yield response.follow(next_page_url, callback=self.parse)
