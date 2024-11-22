6387adf61fd6c30fb175063ea04492e451ed79c5
Aditya <156055539+AdityaAdi07@users.noreply.github.com>
2024-11-22 12:21:44 +0530

    Update app.py

--- infogov/infogov/spiders/app.py
+++ infogov/infogov/spiders/app.py
@@ -12,7 +12,7 @@ def index():
         age = int(request.form['age'])
         state = request.form['state']
 
- 
+        
         result = subprocess.run(['scrapy', 'crawl', 'infospider', '-O', 'output.json'], capture_output=True, text=True)
 
         if result.returncode == 0:
@@ -30,23 +30,25 @@ def index():
             data = []
             print("Scrapy command failed. Please check the spider.")
 
+        
         highlighted_data = []
         normal_data = []
 
         for item in data:
             description = item.get('DESCRIPTION', '').lower()
 
-     
+            
             if state.lower() in description:
                 item['highlight_state'] = True
 
+               
                 if str(age) in description:
-                    item['highlight_age'] = True 
+                    item['highlight_age'] = True  
                 highlighted_data.append(item)
             else:
                 normal_data.append(item)
 
- 
+        
         data = highlighted_data + normal_data
 
         return render_template('index.html', data=data)
